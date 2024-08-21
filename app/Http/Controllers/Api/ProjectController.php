79ebf45ea9c5ba81fbc93b48d0910024a88c4854
CristianN01 <cristiannechifor71@gmail.com>
2024-08-21 19:38:51 +0200

    testato nel browser se i dati ricevuti erano i project creati

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -9,13 +9,22 @@ use Illuminate\Http\Request;
 class ProjectController extends Controller
 {
     public function index(){
+
+        $projects = Project::with(  "type", "technologyes")->paginate(20);
+
         return response()->json([
-            'name' => "Michele",
-            'state' => "Italy"
+            'success' => true,
+            'results' => $projects
         ]);
     }
 
     public function show(Project $project){
 
+        $projects = Project::with(  "type", "technologyes")->paginate(20);
+
+        return response()->json([
+            'success' => true,
+            'results' => $projects
+        ]);
     }
 }
